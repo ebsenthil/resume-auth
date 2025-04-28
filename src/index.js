31a7069f749112743d5d6bbbfd3be4f2221f0927
ebsenthil <shri.senthilnathan@gmail.com>
2025-04-28 11:55:29 +0000

    commit auth index.js

--- src/index.js
+++ src/index.js
@@ -14,8 +14,14 @@ Amplify.configure({
     userPoolId: 'us-east-1_UL0HgN1a7', // Your User Pool ID
     userPoolWebClientId: '25naou2ejmro1jobjqkd0chkj0', // Your App Client ID (Web)
     authenticationFlowType: 'USER_SRP_AUTH', // Recommended flow for most apps
-    // --- Add this empty oauth object to prevent the error ---
-    oauth: {},
+    // --- Try this slightly more structured empty oauth object ---
+    oauth: {
+       domain: '', // Can be an empty string or a placeholder if needed
+       scope: [],
+       redirectSignIn: 'http://localhost/', // Use a dummy value like localhost
+       redirectSignOut: 'http://localhost/', // Use a dummy value
+       responseType: 'code' // Or 'token', depending on flow (code is standard)
+    },
     // --- End add ---
   }
   // If you had other Amplify resources (like APIs, etc.), you'd add them here too
@@ -32,7 +38,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your performance, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
